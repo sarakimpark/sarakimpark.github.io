de2c96eda4706b0ad9c64ce04aa384f0eecfeae5
Sara Kim <sarakim323@gmail.com>
2025-11-13 16:15:48 -0600

    rename tag for header description

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,7 +98,7 @@ const mySwiper = new Swiper('.testimonial__container', {
 // gsap.from('.home__img', {opacity: 0, duration: 2, delay: .5, x: 60})
 
 gsap.from('.home__data', {opacity: 0, duration: 2, delay: .8, y: 25})
-gsap.from('.home__greeting, .home__name, .home__profession, .home__button', {opacity: 0, duration: 2, delay: 1, y: 25, ease:'expo.out', stagger: .2})
+gsap.from('.home__greeting, .home__name, .home__description, .home__button', {opacity: 0, duration: 2, delay: 1, y: 25, ease:'expo.out', stagger: .2})
 
 gsap.from('.nav__logo, .nav__toggle', {opacity:0, duration: 2, delay: 1.5, y: 25, ease:'expo.out', stagger: .2});
 gsap.from('.nav__item', {opacity:0, duration: 2, delay: 1.8, y: 25, ease:'expo.out', stagger: .2});
